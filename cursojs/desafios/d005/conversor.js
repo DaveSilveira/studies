e270ada5df0a7cdbf0578da69776e488996f018a
David Jordânio Silveira <119808305+DaveSilveira@users.noreply.github.com>
2022-12-13 18:16:46 -0300

    verificação de erros

--- cursojs/desafios/d005/conversor.js
+++ cursojs/desafios/d005/conversor.js
@@ -1,9 +1,20 @@
+window.alert('Teste');
+
 function converter(){
 
-var valor = document.getElementById(valor);
-var km = valor  
-var  = 
-var  = 
+var valor = Number(document.getElementById(valor));
 
-document.write();
+console.log(typeof(valor));
+
+var mm = valor / 1000;
+var cm = valor / 100;
+var dm = valor / 10;
+
+var Dam = valor * 10;
+var Hm = valor * 100;
+var km = valor * 1000;
+
+alert(`A distância de ${valor} metros corresponde a: \n  
+ ${mm} Milimetros \n ${cm} Centimetros \n ${dm} Decimetros \n ${Dam} Dacametros \n ${Hm} Hectômetros \n ${km} Quilometros`)
+
 }
