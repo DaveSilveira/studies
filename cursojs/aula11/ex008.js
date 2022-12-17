7db199ed85a7e26b86371594866026b922568635
David Jordânio Silveira <119808305+DaveSilveira@users.noreply.github.com>
2022-12-16 23:29:42 -0300

    Condições simples

---
+++ cursojs/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 75.9;
+console.log(`A velocidade é de ${vel}`);
+if (vel >= 60){ //Condição simples
+    console.log(`Você ultrapassou o limite de velocidade (${vel + ' km/h'}). Foi gerada uma multa!`);
+}
+console.log(`Dirija sempre usando o cinto de segurança`);
---
+++ cursojs/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil';
+console.log(`Vivendo em ${pais}`);
+if (pais != 'Brasil'){
+    console.log('Você é estrangeiro.');
+} else { //Adicionando o else a condição passa a ser composta
+    console.log('Você é brasileiro!');
+}
