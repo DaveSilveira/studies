5334e5b2b6aa3d69ad80d9951cba8fc07d65ba01
David Jordânio Silveira <119808305+DaveSilveira@users.noreply.github.com>
2022-12-22 10:29:06 -0300

    adicionando informaçoes, setando valores

--- diluições/diluições.js
+++ diluições/diluições.js
@@ -5,21 +5,37 @@ v - volume (L)
 mol - molaridade (mol/L)
 */
 
-var MM = document.getElementById('MM').value;
-var m = document.getElementById('m').value;
-var vol = document.getElementById('vol').value;
-var mol = document.getElementById('mol').value;
 
-if (massa || Massa){
-	m = mol * MM * vol;
-	window.alert(`O valor da massa é ${m}`);
-} else if (molaridade || Molaridade) {
-	mol = m / (MM * vol);
-	window.alert(`O valor da molaridade é: ${mol}`);
-} else if (volume || Volume){
-	vol =  m / (MM * mol);
-	window.alert(`O valor do volume é: ${vol}`);
-} else if (massa molar || PM || MM){
-	MM = m / (mol * vol);
-	window.alert(`O valor do Peso molecular é de: ${MM}`)
+/*OBSERVAÇÕES
+
+ADICIONAR TEOR E DENSSIDADE E VERIFICAR POR QUE ESTÁ RETORNANDO VALOR NULL QUANDO CAIXA ESTA ZERADA.
+REVERTER ISSO, QUEM SABE COLOCANDO CONDICIONAL NA SELEÇÃO DOS VALORES??
+*/
+
+function enviar(){
+
+var MM = Number(document.getElementById('MM')).value;
+var m = Number(document.getElementById('m')).value;
+var vol = Number(document.getElementById('vol')).value;
+var mol = Number(document.getElementById('mol')).value;
+var teor = Number(document.getElementById('teor')).value;
+var densi = Number(document.getElementById('densi')).value;
+var res = document.getElementById('res');
+var und = document.getElementsByName('und');
+
+	if (und[2].checked){
+		m = mol * MM * vol;
+		res.innerHTML = `O valor da massa é ${m}`;
+	} else if (und[0].checked) {
+		mol = m / (MM * vol);
+		res.innerHTML =`O valor da molaridade é: ${mol}`;
+	} else if (und[1].checked){
+		vol =  m / (MM * mol);
+		res.innerHTML = `O valor do volume é: ${vol}`;
+	} else if (und[3].checked){
+		MM = m / (mol * vol);
+		res.innerHTML = `O valor do Peso molecular é de: ${MM}`;
+	}
+res.style.textAlign = 'center';
+res.style.backgroundColor = 'green'
 }
