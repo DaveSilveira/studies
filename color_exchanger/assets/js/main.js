21d8b2f24909571671e71b32de95600a560f7393
David Jordânio Silveira <119808305+DaveSilveira@users.noreply.github.com>
2023-01-06 15:31:12 -0300

    Add files via upload

--- color_exchanger/assets/js/main.js
+++ color_exchanger/assets/js/main.js
@@ -4,39 +4,21 @@ function criaCaixa(){
 	const caixa = document.createElement('div');
 	caixa.style.width = '20px';
 	caixa.style.height = '20px';
-	caixa.style.margin = '2px';
+	caixa.style.margin = 'auto';
+	caixa.style.borderRadius = '50px';
 	return caixa;
 }
 
+
 function lancaCaixa(){ //É necessário criar a função e chamala, pois com o innerHTML ele imprime o objeto div e não a caixa em si
-let caixaMae = criaCaixa();
-corpo.appendChild(caixaMae);
-caixaMae.style.margin = 'auto';
+let caixa = criaCaixa();
+corpo.appendChild(caixa);
+caixa.style.margin = 'auto';
+caixa.style.backgroundColor = 'red';
 
-let vermelho = criaCaixa();
-caixaMae.appendChild(vermelho);
-vermelho.style.backgroundColor = 'red'
-
-let azul = criaCaixa();
-caixaMae.appendChild(azul);
-azul.style.backgroundColor = 'blue'
-
-let verde = criaCaixa();
-caixaMae.appendChild(verde);
-verde.style.backgroundColor = 'green'
-
-let preto = criaCaixa();
-caixaMae.appendChild(preto);
-preto.style.backgroundColor = 'black'
-
-	function mudaCor(){ //melhor jeito para mudar a cor?
-		const mudaCor = document.body.style.backgroundColor = '';
-		return mudaCor;
-	}
-
-//criar interação ao clique nas divs
-
-return preto, verde, azul, vermelho;
+return caixa;
 }
 
+
+lancaCaixa();
 lancaCaixa();
