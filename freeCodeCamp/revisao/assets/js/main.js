007a45f192cd32d16c845928dd96ee6aa3d4dffd
David Jordânio Silveira <davidjvsilveira@gmail.com>
2023-07-06 18:24:50 -0300

    começando

--- freeCodeCamp/revisao/assets/js/main.js
+++ freeCodeCamp/revisao/assets/js/main.js
@@ -10,18 +10,24 @@ function criaP(){
 }
 
 const review = [
-    {
-        foto: '',
-        nome: '',
-        profissao: '',
-        comentario: '',
-
-    }
+    {foto: '', nome: 'Dave', profissao: 'Biomedico', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+    {foto: '', nome: 'Julius', profissao: 'Contador', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+    {foto: '', nome: 'Clara', profissao: 'Advogada', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
 ]
 
 corpo.addEventListener('DOMContentLoaded', function(){
+    corpo.innerText = 'começando'
+    /*
+    review.forEach()
+    
     let foto = criaDiv()
     let nome = criaP()
     let profissao = criaP()
     let comentario = criaP()
+
+    let antes = '<'
+    let proximo = '>'
+    let aleatorio = 'suprise-me'
+
+*/
 });
