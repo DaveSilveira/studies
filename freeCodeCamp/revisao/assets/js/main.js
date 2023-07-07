1490719e5c6fae2e21b5c1cd2394ba7c629f89a1
David Jordânio Silveira <davidjvsilveira@gmail.com>
2023-07-07 00:18:05 -0300

    mexendo no algoritmo

--- freeCodeCamp/revisao/assets/js/main.js
+++ freeCodeCamp/revisao/assets/js/main.js
@@ -14,20 +14,30 @@ const review = [
     {foto: '', nome: 'Julius', profissao: 'Contador', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
     {foto: '', nome: 'Clara', profissao: 'Advogada', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
 ]
-
-corpo.addEventListener('DOMContentLoaded', function(){
-    corpo.innerText = 'começando'
-    /*
-    review.forEach()
-    
+function mostrarQuadro(){
+    let div = criaDiv()
     let foto = criaDiv()
+    foto.innerHTML = review[0]['foto']
+    div.appendChild(foto)
     let nome = criaP()
+    nome.innerText = review[0]['nome'].textContent
+    div.appendChild(nome)
     let profissao = criaP()
+    profissao.innerText = review[0]['profissao']
+    div.appendChild(profissao)
     let comentario = criaP()
+    comentario.innerText = review[0]['comentario']
+    div.appendChild(comentario)
 
+    return div;
+        }
+
+document.addEventListener('DOMContentLoaded', function(){
+    let comentario = criaDiv()
+    corpo.appendChild(comentario)
+    comentario.innerHTML = mostrarQuadro()
     let antes = '<'
     let proximo = '>'
     let aleatorio = 'suprise-me'
 
-*/
 });
