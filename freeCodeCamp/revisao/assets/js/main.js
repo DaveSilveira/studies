9641aac81946f4a13660fbc53063d5ebead50897
David Jordânio Silveira <davidjvsilveira@gmail.com>
2023-07-08 15:55:02 -0300

    trabalhando no algoritmo

--- freeCodeCamp/revisao/assets/js/main.js
+++ freeCodeCamp/revisao/assets/js/main.js
@@ -1,43 +1,26 @@
-const corpo = document.querySelector('.corpo')
-
-function criaDiv(){
-    let div = document.createElement('div')
-    return div
-}
-function criaP(){
-    let p = document.createElement('p')
-    return p
-}
+let foto = document.querySelector('.foto')
+let nome = document.querySelector('.nome')
+let profissao = document.querySelector('.profissao')
+let comentario = document.querySelector('.comentario')
 
 const review = [
-    {foto: '', nome: 'Dave', profissao: 'Biomedico', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
-    {foto: '', nome: 'Julius', profissao: 'Contador', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
-    {foto: '', nome: 'Clara', profissao: 'Advogada', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
+    {foto: 'URL(./assets/img/userM.png)', nome: 'Dave', profissao: 'Biomedico', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+    {foto: 'URL(./assets/img/userM.png)', nome: 'Julius', profissao: 'Contador', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+    {foto: 'URL(./assets/img/userF.png)', nome: 'Clara', profissao: 'Advogada', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
 ]
-function mostrarQuadro(){
-    let div = criaDiv()
-    let foto = criaDiv()
-    foto.innerHTML = review[0]['foto']
-    div.appendChild(foto)
-    let nome = criaP()
-    nome.innerText = review[0]['nome'].textContent
-    div.appendChild(nome)
-    let profissao = criaP()
-    profissao.innerText = review[0]['profissao']
-    div.appendChild(profissao)
-    let comentario = criaP()
-    comentario.innerText = review[0]['comentario']
-    div.appendChild(comentario)
 
-    return div;
-        }
+const btnVolta= document.querySelector('.volta')
+const btnFrente = document.querySelector('.frente')
+const aleatorio = document.querySelector('.aleatorio')
+let item = 0;
 
 document.addEventListener('DOMContentLoaded', function(){
-    let comentario = criaDiv()
-    corpo.appendChild(comentario)
-    comentario.innerHTML = mostrarQuadro()
-    let antes = '<'
-    let proximo = '>'
-    let aleatorio = 'suprise-me'
 
+    foto.style.backgroundImage = review[item].foto
+    nome.textContent = review[item].nome
+    profissao.textContent = review[item].profissao
+    comentario.textContent = review[item].comentario
 });
+btnFrente.addEventListener('click', ()=> item++)
+btnFrente.addEventListener('click', ()=> item - 1)
+aleatorio.addEventListener('click', ()=> item = Math.floor(Math.random() * review.length))
