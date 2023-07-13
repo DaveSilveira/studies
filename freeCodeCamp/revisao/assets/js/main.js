2809bdeba776130343ddbc0679b0f0b0d8c561fe
David Jordânio Silveira <davidjvsilveira@gmail.com>
2023-07-13 01:40:42 -0300

    ajustando algoritmo

--- freeCodeCamp/revisao/assets/js/main.js
+++ freeCodeCamp/revisao/assets/js/main.js
@@ -6,7 +6,10 @@ let comentario = document.querySelector('.comentario')
 const review = [
     {foto: 'URL(./assets/img/userM.png)', nome: 'Dave', profissao: 'Biomedico', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
     {foto: 'URL(./assets/img/userM.png)', nome: 'Julius', profissao: 'Contador', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
-    {foto: 'URL(./assets/img/userF.png)', nome: 'Clara', profissao: 'Advogada', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
+    {foto: 'URL(./assets/img/userF.png)', nome: 'Clara', profissao: 'Advogada', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+    {foto: 'URL(./assets/img/userF.png)', nome: 'Márcia', profissao: 'Cantora', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+    {foto: 'URL(./assets/img/userF.png)', nome: 'Valeria', profissao: 'Farmaceutica', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'},
+    {foto: 'URL(./assets/img/userM.png)', nome: 'Marcelo', profissao: 'Assistente de RH', comentario: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
 ]
 
 const btnVolta= document.querySelector('.volta')
@@ -23,16 +26,26 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 btnFrente.addEventListener('click', ()=>{
     item = ++item
+    if(item >= review.length) item = 0
     foto.style.backgroundImage = review[item].foto
     nome.textContent = review[item].nome
     profissao.textContent = review[item].profissao
     comentario.textContent = review[item].comentario
     console.log(item)
 })
-btnFrente.addEventListener('click', ()=> item = item - 1)
+btnVolta.addEventListener('click', ()=> {
+    item = --item 
+    console.log(item)
+    if(item == 0) item = review.length //verificar
+    foto.style.backgroundImage = review[item].foto
+    nome.textContent = review[item].nome
+    profissao.textContent = review[item].profissao
+    comentario.textContent = review[item].comentario
+    
+})
+
 aleatorio.addEventListener('click', ()=> {
     item = Math.floor(Math.random() * review.length)
-    
     foto.style.backgroundImage = review[item].foto
     nome.textContent = review[item].nome
     profissao.textContent = review[item].profissao
