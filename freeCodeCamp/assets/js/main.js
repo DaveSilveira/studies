5cff7ac78686f8b296b5b5ace9c8a7406f3ffb06
David Jordânio Silveira <davidjvsilveira@gmail.com>
2023-07-17 02:35:37 -0300

    ajustando botão de seleção de tema

--- freeCodeCamp/assets/js/main.js
+++ freeCodeCamp/assets/js/main.js
@@ -2,25 +2,34 @@ const navbar = document.querySelector('.navbar');
 const links = document.getElementsByTagName('a')
 const escopo = document.querySelector('.escopo-geral')
 
-function criaCaixa(){
-	const caixa = document.createElement('div');
-    caixa.style.cssText = `width:10px; height:10px; margin:2px 2px auto; border-radius:50px; 
-    border:1px solid #ffff; display:inline-block;`
-	return caixa;
+function criaDiv(){
+    let div = document.createElement('div')
+    return div;
 }
-function themeSelector(fundo, primeiroPlano, fonte,){
-    let cor = criaCaixa()
+
+const tema = criaDiv()
+navbar.appendChild(tema)
+tema.innerText = 'Tema';
+tema.classList.add('botao')
+
+function themeSelector(fundo, primeiroPlano, fonte, nome){
+    let cor = criaDiv()
+    let nomeCor = criaDiv()
+    navbar.appendChild(nomeCor)
     navbar.appendChild(cor)
-    cor.style.backgroundColor = fundo;
+    cor.style.cssText = `width:10px; height:10px; margin:2px 2px auto; border-radius:50px; 
+    border:1px solid #ffff; display:block; background-color:${fundo}`
+
     cor.addEventListener('click', () => {
         document.body.style.backgroundColor = fundo;
         document.body.style.color = fonte;
         links.style.backgroundColor = fonte;
         navbar.style.backgroundColor = primeiroPlano;
         escopo.style.backgroundColor = primeiroPlano;
-    }, false)
-
+    });
+    nomeCor.innerText = nome;
 }
-
-themeSelector('#000000', '#363636','#EEEEEE')
-themeSelector('#FFFFFF', '#EEFEEE', '#262626')
+tema.addEventListener('click', function(){
+themeSelector('#000000', '#363636','#EEEEEE','Noite');
+themeSelector('#FFFFFF', '#EEFEEE', '#262626', 'Dia');
+})
