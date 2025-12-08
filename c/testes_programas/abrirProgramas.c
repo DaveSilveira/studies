741077eb5aa7def7cfe4322887b7c90ac8e1c78a
David Jordânio Silveira <119808305+DaveSilveira@users.noreply.github.com>
2025-12-08 12:57:26 -0300

    começando a escrever algumas coisinhas em C

---
+++ c/testes_programas/abrirProgramas.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("ABRIR VARIOS PROGRAMAS JUNTOS\n");
+    printf("Inicializando arquivos de texto\n");
+    system("pause");
+    system("start teste.txt");
+    system("start teste2.txt");
+}
